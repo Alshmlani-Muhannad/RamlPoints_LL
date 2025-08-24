57c9f97e89d75d73eccbff4beca63d2c05894fa4
Alshmlani Muhannad <148769908+Alshmlani-Muhannad@users.noreply.github.com>
2025-08-24 22:33:03 +0000

    G

--- js/translations.js
+++ js/translations.js
@@ -184,9 +184,7 @@ const translations = {
         Terms_Privacy_Link: "Terms of Service & Privacy Policy",
         Send_Message_ll:"Send Message",
         We_will_respond:"We'll respond within 24 hours",
-        We_r_here:"We're Here to Help",
-        Contact_RamlPoints_ii:"Contact",
-        RamlPoints_klz:"RamlPoints"
+        We_r_here:"We're Here to Help"
 
 
 
@@ -388,9 +386,7 @@ const translations = {
         Terms_Privacy_Link: "شروط الخدمة وسياسة الخصوصية",
         Send_Message_ll:"ارسل الرسالة",
         We_will_respond:"سوف نقوم بالرد خلال 24 ساعة",
-        We_r_here:"نحن هنا للمساعدة",
-        Contact_RamlPoints_ii:" تواصل",
-        RamlPoints_klz:"رمل بوينتس"
+        We_r_here:"نحن هنا للمساعدة"
         
 
         
